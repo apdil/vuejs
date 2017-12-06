4ce18191e4420c87956b2fd6dbb48efdb04ae733
Apdil <apdil.aydinalp@gmail.com>
2017-12-06 21:59:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+let lol = new Vue({
+    el: "#lol",
+    data: {
+        message: 'tarée'
+    }
+})
